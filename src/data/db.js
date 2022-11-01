5c839811cc016fba95a729f2e07581707ef142ee
Samira <samira@Samiras-MacBook-Air.local>
2022-11-01 14:45:00 +0330

    fixed bug

--- src/data/db.js
+++ src/data/db.js
@@ -1,6 +1,6 @@
 const todos = [
-    { id: 1,"startDate": new Date().toLocaleString(), title: "Title", description: "say what to do ",isDone: false, },
-    { id: 2,"startDate": new Date().toLocaleString(), title: "Title", description: "say what to do ",isDone: false, }
+    { id: 1,"startDate": new Date().toLocaleString(),"endDate": new Date().toLocaleString(), title: "Title", description: "say what to do ",isDone: false, },
+    { id: 2,"startDate": new Date().toLocaleString(),"endDate": new Date().toLocaleString(), title: "Title", description: "say what to do ",isDone: false, }
 ];
 
 export default todos;
